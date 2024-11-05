baa2c00fe5b9a6ba395e1aacfac20c88b634ccfb
Administrator <bahrom1982@gmail.com>
2024-11-05 12:48:26 +0500

    commit

---
+++ src/main/kotlin/uz/ifood/app/v1/config/AppConfig.kt
@@ -0,0 +1,18 @@
+package uz.ifood.app.v1.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import java.beans.BeanProperty
+
+@Configuration
+class AppConfig {
+
+//    @Bean
+//    @DependsOn("flyway")
+//    fun entityManagerFactory() : LocalContainerEntityManagerFactoryBean{
+//        val emf = LocalContainerEntityManagerFactoryBean();
+//        return emf;
+//    }
+}
